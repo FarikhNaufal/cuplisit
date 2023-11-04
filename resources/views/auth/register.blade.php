18e48e961a7211f10d50a1092ab6b147dcf97965
DESKTOP-7P7LQ5J\Naufal <muhammad.tajuddin20@student.uisi.ac.id>
2023-11-04 12:45:45 +0700

    adding recaptcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,13 +12,13 @@
                 <p class="text-white">Register Form</p>
                 <input type="text"
                     class="mt-3 mb-2 placeholder:text-opacity-60 placeholder:text-primary rounded-md w-full px-2 py-1"
-                    placeholder="Username" name="username">
+                    placeholder="Username" name="username" value="{{old('username')}}">
                 @error('username')
                     <label class="text-red-600">{{ $message }}</label>
                 @enderror
                 <input type="email"
                     class="mt-3 mb-2 placeholder:text-opacity-60 placeholder:text-primary rounded-md w-full px-2 py-1"
-                    placeholder="Email" name="email">
+                    placeholder="Email" name="email" value="{{old('email')}}">
                 @error('email')
                     <label class="text-red-600">{{ $message }}</label>
                 @enderror
@@ -55,26 +55,35 @@
                 <div class="flex items-center justify-between">
                     <div class="flex flex-col gap-y-2">
                         <input type="date" class="mt-3 mb-2 text-primary rounded-md px-2 py-1" placeholder="Email"
-                            name="dob">
+                            name="dob" value="{{old('dob')}}">
                         @error('dob')
                             <label class="text-red-600">{{ $message }}</label>
                         @enderror
                     </div>
                     <div class="flex flex-col">
-                        <select name="gender" class="text-primary px-2 py-1 rounded-md">
+                        <select name="gender" class="text-primary px-2 py-1 rounded-md ">
                             <option value="male">Male</option>
                             <option value="female">Female</option>
+                            <option value="undefined">Undefined</option>
                         </select>
                         @error('gender')
                             <label class="text-red-600">{{ $message }}</label>
                         @enderror
                     </div>
                 </div>
-                <div class="flex gap-1 mt-2 mb-7">
+                <div class="flex gap-1 mt-2">
                     <input type="checkbox" name="" id="">
                     <span class="text-white text-xs">Remember me</span>
                 </div>
-                <button class="w-full py-4 text-white rounded-md bg-primary mb-2 hover:bg-opacity-60">LOGIN</button>
+
+                <div class="g-recaptcha mt-2" data-sitekey="{{config('services.recaptcha.key')}}">
+                </div>
+                @error('g-recaptcha')
+                    <label class="text-red-600">{{ $message }}</label>
+                @enderror
+
+
+                <button class="w-full py-4 text-white rounded-md bg-primary mt-7 mb-2 hover:bg-opacity-60">REGISTER NOW</button>
                 <a href="login" class="mx-auto text-white text-sm hover:underline hover:underline-offset-2">I already have
                     an account</a>
             </form>
@@ -117,4 +126,5 @@
         password.focus()
     });
 </script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
 @endsection
